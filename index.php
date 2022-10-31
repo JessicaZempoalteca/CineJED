05d18e408e711debf9b2169e65c19e338ea46156
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-10-31 11:28:30 -0600

    header image

--- index.php
+++ index.php
@@ -46,10 +46,11 @@
         </nav>
         <!-- Masthead-->
         <header class="masthead">
-            
+            <div class="container">
                 <div class="masthead-subheading">Welcome To Our Cinema!</div>
                 <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
                 <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+            </div>
         </header>
 
         <!-- Estrenos-->
