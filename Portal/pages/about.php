846411fab6deb9e901176c0829942cd6e8e2cfe0
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-07 09:06:26 -0600

    actualizacion about

---
+++ Portal/pages/about.php
@@ -0,0 +1,255 @@
+<?php
+require('../../helpers/menu_portal.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Conócenos</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="./assets/Vector-Cinema-Projector-PNG-File.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../resources/css/styles.css" rel="stylesheet">
+        <link rel="stylesheet" href="../resources/css/estilos.css">
+        <link rel="shortcut icon" href="../img/boletosLogo.png">
+    </head>
+    <body id="page-top">
+
+        <!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" style="background-color: #010101;">
+            <div class="container nav">
+                <a class="navbar-brand" href="#page-top"><img src="../img/cineLogo.png" alt="..." /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <?php echo crear_menu('pages','about');?>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Masthead-->
+        <header">
+        </header>
+        <br>
+        <br>
+        <br>
+
+        <!-- About-->
+        <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">CineJED</h2>
+                    <h3 class="section-subheading text-muted">Conoce más sobre nuestro cine</h3>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image">
+                            <h1>
+                                <br>
+                                C
+                            </h1>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Nosotros</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">CineJED empresa líder en entretenimiento, con más de 5 años en el mercado, se encuentra posicionado en el Top 20 de las mejores cadenas cinematográficas del mundo. </p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h1>
+                                <br>
+                                i
+                            </h1>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Misión</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">Nuestra misión es dar a nuestros invitados la mejor experiencia de entretenimiento, basada en el esfuerzo, talento y compromiso de todos, generando valor, innovación y mucha diversión. </p></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="timeline-image">
+                            <h1>
+                                <br>
+                                n
+                            </h1>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Visión</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">Ser la opción de entretenimiento preferida por nuestros invitados en los países en donde operamos; reconocida por la ubicación, diseño y confort de las instalaciones, la calidad y variedad de los productos y servicios que ofrecemos, la continua innovación tecnológica y la satisfacción de todos nuestros invitados.</p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h1>
+                                <br>
+                                e
+                            </h1>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Nuestros valores</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">
+                            <strong>Integridad:</strong>
+                                Atendiendo siempre a la verdad, cumpliendo con lo que ofrecemos y esmerándonos en lograrlo. <br>
+
+                            <strong>Compromiso: </strong>
+                            Nos enfocamos en lograr altos estándares de desempeño, demostrando y fomentando una actitud de esfuerzo y responsabilidad   
+                            </p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h1>
+                                <br />
+                                JED
+                            </h1>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <!-- Team-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
+                    <h3 class="section-subheading text-muted">Ninguno de nosotros es tan bueno como todos nosotros juntos.</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../../Portal/img/team/Jess.jpeg" alt="..." />
+                            <h4>Jessica Melina Zempoalteca Flores</h4>
+                            <p class="text-muted">Programadora y Analista</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com" target="_blank" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com" target="_blank" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../../Portal/img/team/Ema.jpeg" alt="..." />
+                            <h4>Emanuel González Lira</h4>
+                            <p class="text-muted">Diseñador</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com" target="_blank" aria-label="Diana Petersen Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com" target="_blank" aria-label="Diana Petersen Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="../../Portal/img/team/Dani.jpeg" alt="..." />
+                            <h4>Daniel Flores Hernández</h4>
+                            <p class="text-muted">Analista</p>
+                            <a class="btn btn-dark btn-social mx-2" href="https://twitter.com" target="_blank" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com" target="_blank" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">"Nunca dejes que nadie te diga que no puedes hacer algo. Ni siquiera yo. Si tienes un sueño, tienes que protegerlo. Las personas que no son capaces de hacer algo por ellos mismos, te dirán que tú tampoco puedes hacerlo. ¿Quieres algo? Ve por ello y punto".</p></div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!-- Footer-->
+        <footer class="py-2 bg-dark text-write">
+            <div class="container no-width">
+                <div class="row no-gutters my-5">
+                    <div class="col-12 col-lg-4">
+                        <div class="pr-lg-5 pb-5">
+                            <p class="h1 sc-bwzfXH cEFPpN">
+                            <small>Atención telefónica</small>
+                            <br>
+                            <a href="tel:5552576969" class="text-white">
+                            <strong>246 175 9822</strong>
+                            </a>
+                            </p>
+                        </div>
+                        <div class="pr-lg-5 pt-5 border-top border-secondary">
+                            <div class="d-flex justify-content-between">
+                                <a class="btn btn-outline-white btn-icon-circle" href="http://www.facebook.com/pages/Cinemex/83988183288" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-facebook-f">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://twitter.com/cinemex" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-twitter">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://instagram.com/cinemex" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-instagram">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://www.youtube.com/channel/UCK7DXWnJJarFsyu3-kADUsg" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-youtube">
+                                </i>
+                                </a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="px-5 py-5 py-lg-0 my-5 my-lg-0 sc-bxivhb hgSmeg">
+                            <ul class="font-weight-light small py-2">
+                            <li class="nav-item">
+                            <a class="nav-link py-1 text-white" href="./about.php">Sobre CineJED</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Términos y condiciones Cinefan</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Política de precios</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Política de reembolsos</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="http://cinemex.bumeran.com.mx/" target="_blank" rel="noopener noreferrer">Bolsa de trabajo corporativo</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Términos y condiciones</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Aviso de Privacidad</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Bolsa de trabajo cines</a></li></ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="d-flex justify-content-around justify-content-lg-between align-items-center pl-lg-5 pb-5">
+                            <a class="sc-bdVaJa jeNtWj" href="https://itunes.apple.com/mx/app/cinemex/id418163740" target="_blank" rel="noopener noreferrer">
+                            <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-app-store.png" class="img-fluid1" alt="App Store"></a>
+                            <a class="sc-bdVaJa jeNtWj" href="https://play.google.com/store/apps/details?id=com.cinemex&amp;hl=es" target="_blank" rel="noopener noreferrer">
+                            <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-google-play.png" class="img-fluid1" alt="Google Play"></a>
+                        </div>
+                        <div class="pt-5 border-top border-secondary">
+                            <div class="d-flex flex-wrap flex-md-nowrap justify-content-around justify-content-lg-between align-items-center pl-lg-5">
+                                <a href="https://www.fundaciongrupomexico.org/programas/Paginas/concienciaCinemex.aspx" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-conciencia-cinemex.png" class="img-fluid1" alt="Conciencia Cinemex"></a>
+                                <a href="http://www.canacine.org.mx/" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-canacine.png" class="img-fluid1" alt="Canacine"></a>
+                                <a href="http://www.alboa.com.mx/?utm_source=cinemex" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-alboa.png" class="img-fluid1" alt="Alboa"></a>
+                                <a href="https://gamersarena.com.mx/?utm_source=cinemex" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-arena.png" class="img-fluid1" alt="Gamers Arena"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="../resources/js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
