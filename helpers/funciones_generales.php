5ff97b645cb8fefede55bfee9ea01c9647c0dbaa
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-07 09:04:26 -0600

    actualizacion funciones generales

--- helpers/funciones_generales.php
+++ helpers/funciones_generales.php
@@ -1,5 +1,10 @@
 <?php
     function path_image($folder = '', $img = ''){
-        $path = ($folder != "") ? '../img' . $img : './Portal/img/' . $img;
+        $path = ($folder != "") ? '../img/' . $img : './Portal/img/' . $img;
         return (file_exists($path) ? $path : ($folder != "" ? '../img/noImage.jpg' : './Portal/img/noImage.jpg'));
+    }
+
+    function path_image2($folder = '', $img = ''){
+        $path = ($folder != "") ? '../img/Familiares/' . $img : './Portal/img/Familiares/' . $img;
+        return (file_exists($path) ? $path : ($folder != "" ? '../img/Familiares/noImage.jpg' : './Portal/img/Familiares/noImage.jpg'));
     }
