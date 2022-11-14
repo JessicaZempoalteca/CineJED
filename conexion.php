d829cacfd17ee5180a2b2fcfd5471c26e704514d
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-14 00:18:09 -0600

    archivos de conexion

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+    //Declaración de variables par la conexion con la BD
+    $server = 'localhost'; //Servidor
+    $bd =  'cinejed'; //Base de datos
+    $user = 'root'; //Usuario de acceso a la BD
+    $password = ''; // Contraseña de acceso a la BD
+
+    //Proceso de conexión a la BD
+    $conexion = mysqli_connect($server, $user, $password, $bd);
+
+    //Validamos la conexión con la BD
+    if(!$conexion){
+        die('Error al conectarse con la Base de Datos,'. mysqli_connect_error());
+        exit;
+    }//end 
+    
+    //Procesamo o relizamos una petición a la BD
+    echo '<script>alert("Conexión éxitosa a la BD")</script>';
+    mysqli_query($conexion, 'SET NAMES "utf8"');
+    
