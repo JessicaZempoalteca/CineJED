4033715a764621c2ea933723de2d955ac31b8a71
Ema788 <emagonzalez711@gmail.com>
2022-11-15 09:46:21 -0600

    Create usuarios.php

---
+++ Panel/pages/usuarios.php
@@ -0,0 +1,155 @@
+<?php
+    require('../helpers/menu_portal.php');
+?>
+
+<!--Sirve para comentar-->
+<!Doctype>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Conócenos</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="./assets/Vector-Cinema-Projector-PNG-File.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../resources/css/styles.css" rel="stylesheet">
+        <link rel="stylesheet" href="../resources/css/estilos.css">
+        <link rel="shortcut icon" href="../img/boletosLogo.png">
+    </head> 
+
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" style="background-color: #010101;">
+            <div class="container nav">
+                <a class="navbar-brand" href="#page-top"><img src="../img/cineLogo.png" alt="..." /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <?php echo crear_menu('pages','about');?>
+                </div>
+            </div>
+        </nav>
+
+
+        <center>
+            <h1>Usuarios</h1>
+        </center>
+        <hr>
+        <section>
+            <table>
+                <tr>
+                    <th colspan="6"></thcolspan><center><h4><font color="black" face="Times New Roman">Ofertas de casas</h4></center></th>
+                </tr>
+                <tr>
+                    <th>#</th>
+                </tr>
+
+                <tr>
+                    <td>Usuario</td>
+                </tr>
+
+                <tr>
+                 <td>Rol</td>
+                </tr>
+
+                <tr>
+                    <td>Acciones</td>
+                </tr>
+            </table>
+        </section>
+
+    <br>
+    <br>
+    
+        <!-- Footer-->
+        <footer class="py-2 bg-dark text-write">
+            <div class="container no-width">
+                <div class="row no-gutters my-5">
+                    <div class="col-12 col-lg-4">
+                        <div class="pr-lg-5 pb-5">
+                            <p class="h1 sc-bwzfXH cEFPpN">
+                            <small>Atención telefónica</small>
+                            <br>
+                            <a href="tel:5552576969" class="text-white">
+                            <strong>246 175 9822</strong>
+                            </a>
+                            </p>
+                        </div>
+                        <div class="pr-lg-5 pt-5 border-top border-secondary">
+                            <div class="d-flex justify-content-between">
+                                <a class="btn btn-outline-white btn-icon-circle" href="http://www.facebook.com/pages/Cinemex/83988183288" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-facebook-f">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://twitter.com/cinemex" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-twitter">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://instagram.com/cinemex" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-instagram">
+                                </i>
+                                </a>
+                                <a class="btn btn-outline-white btn-icon-circle" href="https://www.youtube.com/channel/UCK7DXWnJJarFsyu3-kADUsg" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-fw fa-youtube">
+                                </i>
+                                </a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="px-5 py-5 py-lg-0 my-5 my-lg-0 sc-bxivhb hgSmeg">
+                            <ul class="font-weight-light small py-2">
+                            <li class="nav-item">
+                            <a class="nav-link py-1 text-white" href="./about.php">Sobre CineJED</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Términos y condiciones Cinefan</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Política de precios</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Política de reembolsos</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="http://cinemex.bumeran.com.mx/" target="_blank" rel="noopener noreferrer">Bolsa de trabajo corporativo</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Términos y condiciones</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Aviso de Privacidad</a></li>
+                            <li class="nav-item"><a class="nav-link py-1 text-white" href="./about.php">Bolsa de trabajo cines</a></li></ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="d-flex justify-content-around justify-content-lg-between align-items-center pl-lg-5 pb-5">
+                            <a class="sc-bdVaJa jeNtWj" href="https://itunes.apple.com/mx/app/cinemex/id418163740" target="_blank" rel="noopener noreferrer">
+                            <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-app-store.png" class="img-fluid1" alt="App Store"></a>
+                            <a class="sc-bdVaJa jeNtWj" href="https://play.google.com/store/apps/details?id=com.cinemex&amp;hl=es" target="_blank" rel="noopener noreferrer">
+                            <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-google-play.png" class="img-fluid1" alt="Google Play"></a>
+                        </div>
+                        <div class="pt-5 border-top border-secondary">
+                            <div class="d-flex flex-wrap flex-md-nowrap justify-content-around justify-content-lg-between align-items-center pl-lg-5">
+                                <a href="https://www.fundaciongrupomexico.org/programas/Paginas/concienciaCinemex.aspx" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-conciencia-cinemex.png" class="img-fluid1" alt="Conciencia Cinemex"></a>
+                                <a href="http://www.canacine.org.mx/" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-canacine.png" class="img-fluid1" alt="Canacine"></a>
+                                <a href="http://www.alboa.com.mx/?utm_source=cinemex" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-alboa.png" class="img-fluid1" alt="Alboa"></a>
+                                <a href="https://gamersarena.com.mx/?utm_source=cinemex" target="_blank" rel="noopener noreferrer" class="m-2 mb-4">
+                                <img src="https://s3.amazonaws.com/statics3.cinemex.com/v2/dist/images/logo-arena.png" class="img-fluid1" alt="Gamers Arena"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="../resources/js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+</html>
