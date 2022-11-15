18108481fd5596b3d055b0a647dace66bfcef7e9
Ema788 <emagonzalez711@gmail.com>
2022-11-14 22:13:59 -0600

    Roles
    Tabla roles

--- Panel/backend/admin/validar_Usuario.php
+++ Panel/backend/admin/validar_Usuario.php
@@ -15,54 +15,7 @@
     }//end if empty
     else{
         //Query para verificar si existe el usuario
-        $query_text = 'SELECT * FROM usuarios INNER JOIN roles ON usuarios.id_rol = roles.id_rol  WHERE email_usuario="'.$usuario.'" AND password_usuario = "'.$password.'";';
+        $query_text = 'SELECT * FROM usuarios INNER JOIN roles ON usuarios.idRol = roles.idUsuarioRol  WHERE email_usuario="'.$usuario.'" AND password_usuario = "'.$password.'";';
         // echo $query_text;
 
-        //Creamos la consulta con el query
-        //procesamiento de conexion
-                                            //consulta de informacion
-        $query_res = mysqli_query($conexion, $query_text);
-
-        //Mostramos en pantalla la información de la peticion como prueba
-        //print("<pre>".print_r($query_res, true)."</pre>");
-
-        //proceso de comparacion
-        //cuenta el numero de filas que ya se obtuvieron
-        if(mysqli_num_rows($query_res) == 0){
-            //Muestra una alerta
-            echo '<script>alert("Usuario y/o contraseña incorrectos")</script>';
-            //Destruimos la sesion
-            session_destroy();
-            //Redireccionamos al login de nuevo
-            echo '<script>window.location="../../../User/login.php"</script>';
-        }//end if mysqli_num_rows
-        else{
-            // ec
-            //Se obtienen los datos y se meten en un arreglo asociativo
-            //arreglo temporal
-            //se para el resultado de la conexion
-                                                    //todo el resultado se pasa a un arreglo asociativo
-            $datos = mysqli_fetch_array($query_res, MYSQLI_ASSOC);
-            
-            //Muestra los datos del arreglo asociativo
-            print("<pre>".print_r($datos, true)."</pre>");
-
-            //Se crea el archivo de sesiones
-            //vairable de sesion que se pueden manipular en cualquier parte del proyecto
-            $_SESSION['id_usuario'] = $datos['id_usuario'];
-            $_SESSION['nombre_usuario'] = $datos['nombre_usuario'];
-            $_SESSION['id_rol'] = $datos['id_rol'];
-            $_SESSION['rol'] = $datos['rol'];
-
-            print("<pre>".print_r($_SESSION, true)."</pre>");
-
-            //Se libera el objeto de datos asociativo
-            mysqli_free_result($query_res);
-
-            //Se cierra la conexion
-            mysqli_close($conexion);
-
-            //Se redirecciona a un lugar
-            echo '<script>window.location="../../pages/dashboard_prueba.php"</script>';
-        }//end else
     }//end else empty
