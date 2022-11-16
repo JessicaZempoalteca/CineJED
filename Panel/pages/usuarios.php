2837e1b9d3a594f949337c685548a891fbf391ed
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-16 08:08:21 -0600

    crud update usuarios

---
+++ Panel/pages/usuarios.php
@@ -0,0 +1,9 @@
+<?php
+//se deben mostrar los datos de la tabla usuarios
+//se debe incluir la persona que inicia sesion pero no se debe poder modificar desde la tabla,
+//sino que desde el perfil
+//condicion
+                                //VARIABLE DE SESION
+//where usuarios.id_usuario != "'.$_SESSION['id_usuario'].'"
+
+//VALIDAR STATUS EN TODO
