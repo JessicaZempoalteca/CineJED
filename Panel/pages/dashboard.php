019abcd05ba2b5264376299509c8e09017dea502
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-20 08:08:10 -0600

    actualizacion de la tabla de usuarios
    cambio de variables para la conexion a la base de datos del proyecto

--- Panel/pages/dashboard.php
+++ Panel/pages/dashboard.php
@@ -125,7 +125,7 @@
               </div><!-- /.col -->
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item"><a href="./dashboard.php">Inicio</a></li>
                   <li class="breadcrumb-item active">Dashboard v1</li>
                 </ol>
               </div><!-- /.col -->
