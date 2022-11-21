78162525246e316914b1b22f045f2285f8d817a0
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-20 22:18:15 -0600

    Update funciones_generales.php

--- helpers/funciones_generales.php
+++ helpers/funciones_generales.php
@@ -1,24 +1,26 @@
 <?php
 
-     //Constante para tomar la carpeta de recursos del portal
-     $root_path_portal = './portal/resources/';
-     $root_specific_portal = '../resources/';
- 
-     //Constante para tomar la carpeta de usuario
-     $root_specific_user = './resource/';
- 
-     //Constante para tomar la carpeta de recursos pane
-     $root_specific_panel = '../resources/';
- 
-     //Constante para la imagen del perfil
-     $path_img_panel = '../img/';
+//Constante para tomar la carpeta de recursos del portal
+$root_path_portal = './portal/resources/';
+$root_specific_portal = '../resources/';
 
-    function path_image($folder = '', $img = ''){
-        $path = ($folder != "") ? '../img/' . $img : './Portal/img/' . $img;
-        return (file_exists($path) ? $path : ($folder != "" ? '../img/noImage.jpg' : './Portal/img/noImage.jpg'));
-    }
+//Constante para tomar la carpeta de usuario
+$root_specific_user = './resource/';
 
-    function path_image2($folder = '', $img = ''){
-        $path = ($folder != "") ? '../img/Familiares/' . $img : './Portal/img/Familiares/' . $img;
-        return (file_exists($path) ? $path : ($folder != "" ? '../img/Familiares/noImage.jpg' : './Portal/img/Familiares/noImage.jpg'));
-    }
+//Constante para tomar la carpeta de recursos pane
+$root_specific_panel = '../resources/';
+
+//Constante para la imagen del perfil
+$path_img_panel = '../img/';
+
+function path_image($folder = '', $img = '')
+{
+    $path = ($folder != "") ? '../img/' . $img : './Portal/img/' . $img;
+    return (file_exists($path) ? $path : ($folder != "" ? '../img/noImage.jpg' : './Portal/img/noImage.jpg'));
+}
+
+function path_image2($folder = '', $img = '')
+{
+    $path = ($folder != "") ? '../img/Familiares/' . $img : './Portal/img/Familiares/' . $img;
+    return (file_exists($path) ? $path : ($folder != "" ? '../img/Familiares/noImage.jpg' : './Portal/img/Familiares/noImage.jpg'));
+}
