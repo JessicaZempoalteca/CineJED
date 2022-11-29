d10898c4da55233d832a8f79408a76c306695208
Jessica ZF <99075148+JessicaZempoalteca@users.noreply.github.com>
2022-11-28 19:37:42 -0600

    formato de codigo corregido

--- Panel/pages/dashboard.php
+++ Panel/pages/dashboard.php
@@ -1,190 +1,185 @@
 <?php
-  //Crea el menú
-  require('../../helpers/menu_panel.php');
-  //Importa la ruta dependiendo de la carpeta
-  require('../../helpers/funciones_generales.php');
-  //Se utiliza las variables de sesion
-  session_start();
-  //Validamos si la posicion existe y ya tiene un valor determinado por la consulta
-  if(!isset($_SESSION['idUsuario'])){
-      echo '<script>
+//Crea el menú
+require('../../helpers/menu_panel.php');
+//Importa la ruta dependiendo de la carpeta
+require('../../helpers/funciones_generales.php');
+//Se utiliza las variables de sesion
+session_start();
+//Validamos si la posicion existe y ya tiene un valor determinado por la consulta
+if (!isset($_SESSION['idUsuario'])) {
+  echo '<script>
               alert("Error, no ha iniciado sesión y no se puede redirigir a la página deseada.");
               window.location = "../../User/pages/login.php";
               </script>';
-  }//
+} //
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet"
-      href="<?php echo $root_specific_panel.'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'; ?>">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo $root_specific_panel.'plugins/icheck-bootstrap/icheck-bootstrap.min.css'; ?>">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="<?php echo $root_specific_panel.'plugins/jqvmap/jqvmap.min.css'; ?>">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo $root_specific_panel.'css/adminlte.min.css'; ?>">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet"
-      href="<?php echo $root_specific_panel.'plugins/overlayScrollbars/css/OverlayScrollbars.min.css'; ?>">
-  </head>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="<?php echo $root_specific_panel . 'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'; ?>">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo $root_specific_panel . 'plugins/icheck-bootstrap/icheck-bootstrap.min.css'; ?>">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="<?php echo $root_specific_panel . 'plugins/jqvmap/jqvmap.min.css'; ?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo $root_specific_panel . 'css/adminlte.min.css'; ?>">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="<?php echo $root_specific_panel . 'plugins/overlayScrollbars/css/OverlayScrollbars.min.css'; ?>">
+</head>
 
-  <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
 
-      <!-- Preloader -->
-      <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-      </div>
-
-      <!-- Navbar -->
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fa fa-navicon"></i></a>
-          </li>
-        </ul>
-
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-          <!-- Fullscreen -->
-          <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button" data-toggle="tooltip" data-placement="top"
-              title="Fullscreen">
-              <i class="fa fa-arrows-alt"></i>
-            </a>
-          </li>
-          <!-- Perfil -->
-          <li class="nav-item">
-            <?php 
-            $html = '';
-            $html.='  <a class="nav-link" href="./perfil.php?idUsuario='.$_SESSION["idUsuario"].'" role="button" data-toggle="tooltip" data-placement="top" title="Mi perfil">
-            <i class="fa fa-user"></i></a> ';
-            echo $html; 
-            ?>
-           <!--   <a class="nav-link" href="./perfil.php?idUsuario='.$_SESSION['idUsuario'].'" role="button" data-toggle="tooltip" data-placement="top" title="Mi perfil">
-              <i class="fa fa-user"></i>-->
-            </a>
-          </li>
-          <!-- Logout -->
-          <li class="nav-item">
-            <a class="nav-link" href="../backend/admin/cerrar_sesion.php" role="button" data-toggle="tooltip" data-placement="top" title="Cerrar sesión">
-              <i class="fa fa-lock    "></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.navbar -->
-
-      <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-          <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" -->
-          <!-- style="opacity: .8"> -->
-          <span class="brand-text font-weight-light">C I N E J E D</span>
-        </a>
-
-        <!-- Sidebar -->
-        <div class="sidebar">
-          <!-- Sidebar user panel (optional) -->
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              <img src="<?php echo $_SESSION["imagenPerfil"];?>" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-            <b><a href="#" class="d-block"><?php echo $_SESSION["nombreCompleto"];?></a></b>
-            <a href="#" class="d-block"><?php echo $_SESSION["rol"]; ?></a>
-            </div>
-          </div>
-
-          <!-- Sidebar Menu -->
-          <nav class="mt-2">
-            <?php echo crear_menu_panel('dashboard');?>
-          </nav>
-          <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-      </aside>
-
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 class="m-0">Dashboard</h1>
-              </div><!-- /.col -->
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="./dashboard.php">Inicio</a></li>
-                  <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
-              </div><!-- /.col -->
-            </div><!-- /.row -->
-          </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-
-
-      </div>
-      <!-- /.content-wrapper -->
-      <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-        All rights reserved.
-        <div class="float-right d-none d-sm-inline-block">
-          <b>Version</b> 3.2.0
-        </div>
-      </footer>
-
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-      </aside>
-      <!-- /.control-sidebar -->
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="../img/carga.gif" alt="AdminLTELogo" height="60" width="60">
     </div>
-    <!-- ./wrapper -->
 
-    <!-- jQuery -->
-    <script src="<?php echo $root_specific_panel.'plugins/jquery/jquery.min.js';?>"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="<?php echo $root_specific_panel.'plugins/jquery-ui/jquery-ui.min.js';?>"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-      $.widget.bridge('uibutton', $.ui.button)
-    </script>
-    <!-- Bootstrap 4 -->
-    <script src="<?php echo $root_specific_panel.'plugins/bootstrap/js/bootstrap.bundle.min.js';?>"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script
-      src="<?php echo $root_specific_panel.'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js';?>">
-    </script>
-    <!-- Summernote -->
-    <script src="<?php echo $root_specific_panel.'plugins/summernote/summernote-bs4.min.js';?>"></script>
-    <!-- overlayScrollbars -->
-    <script src="<?php echo $root_specific_panel.'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';?>">
-    </script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo $root_specific_panel.'js/adminlte.js';?>"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo $root_specific_panel.'js/demo.js';?>"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="<?php echo $root_specific_panel.'js/pages/dashboard.js';?>"></script>
-  </body>
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fa fa-navicon"></i></a>
+        </li>
+      </ul>
+
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Fullscreen -->
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button" data-toggle="tooltip" data-placement="top" title="Fullscreen">
+            <i class="fa fa-arrows-alt"></i>
+          </a>
+        </li>
+        <!-- Perfil -->
+        <li class="nav-item">
+          <?php
+          $html = '';
+          $html .= '  <a class="nav-link" href="./perfil.php?idUsuario=' . $_SESSION["idUsuario"] . '" role="button" data-toggle="tooltip" data-placement="top" title="Mi perfil">
+            <i class="fa fa-user"></i></a> ';
+          echo $html;
+          ?>
+          <!--   <a class="nav-link" href="./perfil.php?idUsuario='.$_SESSION['idUsuario'].'" role="button" data-toggle="tooltip" data-placement="top" title="Mi perfil">
+              <i class="fa fa-user"></i>-->
+          </a>
+        </li>
+        <!-- Logout -->
+        <li class="nav-item">
+          <a class="nav-link" href="../backend/admin/cerrar_sesion.php" role="button" data-toggle="tooltip" data-placement="top" title="Cerrar sesión">
+            <i class="fa fa-lock    "></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+        <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" -->
+        <!-- style="opacity: .8"> -->
+        <span class="brand-text font-weight-light">C I N E J E D</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="<?php echo $_SESSION["imagenPerfil"]; ?>" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <b><a href="#" class="d-block"><?php echo $_SESSION["nombreCompleto"]; ?></a></b>
+            <a href="#" class="d-block"><?php echo $_SESSION["rol"]; ?></a>
+          </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <?php echo crear_menu_panel('dashboard'); ?>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="./dashboard.php">Inicio</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+      All rights reserved.
+      <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+      </div>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="<?php echo $root_specific_panel . 'plugins/jquery/jquery.min.js'; ?>"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="<?php echo $root_specific_panel . 'plugins/jquery-ui/jquery-ui.min.js'; ?>"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="<?php echo $root_specific_panel . 'plugins/bootstrap/js/bootstrap.bundle.min.js'; ?>"></script>
+  <!-- Tempusdominus Bootstrap 4 -->
+  <script src="<?php echo $root_specific_panel . 'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'; ?>">
+  </script>
+  <!-- Summernote -->
+  <script src="<?php echo $root_specific_panel . 'plugins/summernote/summernote-bs4.min.js'; ?>"></script>
+  <!-- overlayScrollbars -->
+  <script src="<?php echo $root_specific_panel . 'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'; ?>">
+  </script>
+  <!-- AdminLTE App -->
+  <script src="<?php echo $root_specific_panel . 'js/adminlte.js'; ?>"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?php echo $root_specific_panel . 'js/demo.js'; ?>"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="<?php echo $root_specific_panel . 'js/pages/dashboard.js'; ?>"></script>
+</body>
 
 </html>
